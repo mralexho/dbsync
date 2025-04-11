4e8a526f77e5ed074fd19479b0591435860bac25
Alex Ho <aho@edc.nyc>
2025-04-11 11:10:13 -0400

    connect db-list to S3Client

--- src/Command/DbList.php
+++ src/Command/DbList.php
@@ -2,10 +2,11 @@
 
 namespace App\Command;
 
+use Aws\S3\S3Client;
+use Aws\Exception\AwsException;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -16,13 +17,57 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class DbList extends Command
 {
+  protected function configure(): void
+  {
+    $this
+      ->addOption('region', 'r', InputOption::VALUE_OPTIONAL, 'AWS region', 'us-east-1')
+      ->addOption('s3profile', 'p', InputOption::VALUE_OPTIONAL, 'AWS profile', 'default');
+  }
+
   protected function execute(InputInterface $input, OutputInterface $output): int
   {
     $io = new SymfonyStyle($input, $output);
-    $message = sprintf('Listing all S3 buckets:');
+    $region = $input->getOption('region');
+    $profile = $input->getOption('s3profile');
 
-    $io->success($message);
+    $io->title('Connecting to AWS S3');
+    $io->text(sprintf('Using region: %s and profile: %s', $region, $profile));
 
-    return Command::SUCCESS;
+    try {
+      // Create an S3 client
+      $s3Client = new S3Client([
+        'version' => 'latest',
+        'region'  => $region,
+        'profile' => $profile
+      ]);
+
+      // List all buckets
+      $result = $s3Client->listBuckets();
+      $buckets = $result['Buckets'];
+
+      if (count($buckets) > 0) {
+        $io->success('Found the following S3 buckets:');
+        
+        $tableRows = [];
+        foreach ($buckets as $bucket) {
+          $tableRows[] = [
+            $bucket['Name'],
+            $bucket['CreationDate']->format('Y-m-d H:i:s')
+          ];
+        }
+        
+        $io->table(['Bucket Name', 'Creation Date'], $tableRows);
+      } else {
+        $io->warning('No S3 buckets found in your account.');
+      }
+
+      return Command::SUCCESS;
+    } catch (AwsException $e) {
+      $io->error('AWS Error: ' . $e->getMessage());
+      return Command::FAILURE;
+    } catch (\Exception $e) {
+      $io->error('Error: ' . $e->getMessage());
+      return Command::FAILURE;
+    }
   }
 }
